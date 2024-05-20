28ebf5736e9cdf3b2be530cdca1f2c7736a9ac1b
Evgeny Shurupov <schuevgeny@yandex.ru>
2024-05-20 20:21:27 +0300

    Dependency on geometry-utils is included. Used new functionality in the app class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import figure.Circle;
 import figure.Rectangle;
 import figure.Triangle;
+import utils.GeometryUtils;
 
 public class App {
     public static void main(String[] args) {
@@ -13,5 +14,11 @@ public class App {
         System.out.println("Площадь прямоугольника: " + rectangle.getSquare() + "\nПериметр прямоугольника: " + rectangle.getPerimeter());
         System.out.println(" ");
         System.out.println("Площадь треугольника: " + triangle.getSquare() + "\nПериметр треугольника: " + triangle.getPerimeter());
+        System.out.println(" ");
+
+        double areaInSquareFeet = 100;
+        System.out.println("Площадь в квадратных футах: " + areaInSquareFeet);
+        double areaInSquareMeters = GeometryUtils.convertToSquareMeters(areaInSquareFeet);
+        System.out.println("Площадь в квадратных метрах: " + areaInSquareMeters);
     }
 }
